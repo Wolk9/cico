a09adc0e0df7b8ae83f7240f80588e78c0e60bc1
Martin laptop <info@setisbew.nl>
2023-03-10 18:46:06 +0100

    firebase auth

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { Card } from 'primereact/card';
+import { Auth } from './components/auth';
+         
+
+const App = () => {
+  return (<Card title="CICO">
+    
+      <Auth />
+      
+      </Card>)
+    
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 import App from './App';
-import rootReducer from './redux/reducers';
-import { auth } from './firebase/firebase';
-import { addUser, setUsers } from './redux/actions'
+ 
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
 
-const store = configureStore({
-    reducer: rootReducer,
-    middleware: [thunk],
-  });
-  
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      store.dispatch(addUser(user));
-      store.dispatch(setUsers());
-    }
-  });
+//icons
+import "primeicons/primeicons.css";                                         
+        
+
+
   
   const rootElement = document.getElementById('root');
   
   createRoot(rootElement).render(
-    <Provider store={store}>
+    <div>
       <App />
-    </Provider>
+    </div>
   );
