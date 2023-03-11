126dfb505e45119fe02abb355785c7ab5232a15d
Martin laptop <info@setisbew.nl>
2023-03-11 12:24:58 +0100

    authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-
-import { Card } from 'primereact/card';
-import { Auth } from './components/auth';
-         
+import { Card } from "primereact/card";
+import { Dialog } from "primereact/dialog";
+import { useState } from "react";
+import { Auth } from "./components/auth";
+import { auth } from "./config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const App = () => {
-  return (<Card title="CICO">
-    
-      <Auth />
-      
-      </Card>)
-    
+  //  const [popUpVisible, setPopUpVisible] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [user, setUser] = useState("");
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  if (user) {
+    setShowSignUp(false);
+  } else {
+    setShowSignUp(true);
+  }
+
+  return (
+    <Card title="CICO">
+      <h4>User logged In:</h4>
+      {user?.email}
+      <Dialog
+        header={showSignUp ? "Sign Up" : "Sign In"}
+        visible={showSignUp}
+        style={{ width: "60vw" }}
+      >
+        <Auth showSignUp={showSignUp} setShowSignUp={setShowSignUp} />
+      </Dialog>
+    </Card>
+  );
 };
 
 export default App;
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,55 +1,144 @@
 
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
-import { auth, googleProvider } from '../config/firebase'
-import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
-import { useState } from 'react';
+import { Card } from "primereact/card";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
-export const Auth = () => {
- const [ email, setEmail] = useState("");
- const [ password, setPassword] = useState("")
+export const Auth = (props) => {
+  const { showSignUp, setShowSignUp } = props;
 
-console.log(auth?.currentUser?.email);
+  console.log(showSignUp);
 
- const signIn =  async () => {
-    try {
-        await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-        console.error(err)
-    }
+  console.log(auth?.currentUser?.email);
+
+  return (
+    <div>
+      {!showSignUp ? (
+        <SignInDialog setShowSignUp={setShowSignUp} />
+      ) : (
+        <SignUpDialog setShowSignUp={setShowSignUp} />
+      )}
+    </div>
+  );
 };
 
-const signInWithGoogle = async () => {
-    try {
-        await signInWithPopup(auth, googleProvider)
-    } catch (err) {
-        console.error(err);
-    }
+const SignInDialog = (props) => {
+  const { setShowSignUp } = props;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const signInUser = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signOutUser = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="p-inputgroup">
+        Sign In
+        <span className="p-inputgroup-addon">
+          <i className="pi pi-user"></i>
+          <InputText
+            placeholder="Email..."
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </span>
+        <span className="p-inputgroup-addon">
+          <i className="pi pi-question"></i>
+          <InputText
+            type="password"
+            placeholder="Password..."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </span>
+        <Button label="SignIn" onClick={signInUser} />
+      </div>
+      <div>
+        <Button
+          label="not a user yet?"
+          onClick={() => setShowSignUp(true)}
+          link
+        />
+      </div>
+    </>
+  );
 };
 
-const logout = async () => {
-    try {
-         await signOut(auth)
-    } catch (err) {
-        console.error(err);
-    }
-}
+const SignUpDialog = (props) => {
+  const { setShowSignUp } = props;
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
 
-    return (
-        <div className="p-inputgroup">
-            <span className="p-inputgroup-addon">
-                <i className="pi pi-user"></i>
-                <InputText placeholder="Email..." onChange={(e)=>setEmail(e.target.value)} />
-            </span>
-            <span className="p-inputgroup-addon">
-                <i className="pi pi-question"></i>
-                <InputText type="password" placeholder="Password..." onChange={(e)=>setPassword(e.target.value)}/>
-            </span>
-            <Button label="SignIn" onClick={signIn} />
-            <Button label="SignIn with Google" onClick={signInWithGoogle} />
-            <Button label="Logout" onClick={logout} />
-        
+  const registerUser = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="p-inputgroup">
+        Sign Up
+        <span className="p-inputgroup-addon">
+          <i className="pi pi-user"></i>
+          <InputText
+            placeholder="Register Email..."
+            onChange={(e) => setRegisterEmail(e.target.value)}
+          />
+        </span>
+        <span className="p-inputgroup-addon">
+          <i className="pi pi-question"></i>
+          <InputText
+            type="password"
+            placeholder="Register Password..."
+            onChange={(e) => setRegisterPassword(e.target.value)}
+          />
+        </span>
+        <Button label="SignUp" onClick={registerUser} />
+      </div>
+      <div>
+        <div>
+          <Button
+            label="already an user?"
+            onClick={() => setShowSignUp(false)}
+            link
+          />
         </div>
-    );
+      </div>
+    </>
+  );
 };
