aaf9cbec62559d5dc20f598413260673acd19e32
Martin laptop <info@setisbew.nl>
2023-03-16 20:55:40 +0100

    Nav

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { Button } from "primereact/button";
 import { Menubar } from "primereact/menubar";
 import { Sidebar } from "./components/Sidebar";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Link,
-  Outlet,
-  Route,
-  RouterProvider,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Cico } from "./pages/cico";
 import { User } from "./pages/user";
 import { Admin } from "./pages/admin";
@@ -29,7 +20,12 @@ const App = () => {
   const [user, setUser] = useState("");
 
   onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
+    if (currentUser) {
+      setUser(currentUser);
+    } else {
+      setUser("");
+      navigate("/");
+    }
   });
 
   console.log(user);
@@ -40,33 +36,41 @@ const App = () => {
     console.log("signOutUser");
     try {
       await signOut(auth);
-      navigate("/");
     } catch (err) {
       console.error(err);
     }
   };
 
-  useEffect(() => {
-    if (user?.email !== undefined) {
-      console.log("set popup false");
-      setPopUpVisible(false);
-    } else {
-      console.log("set popup true");
-      setPopUpVisible(true);
-    }
-  }, [user]);
+  const logIn = () => {
+    console.log("logIn");
+    setPopUpVisible(true);
+  };
+
+  // useEffect(() => {
+  //   if (user?.email !== undefined) {
+  //     console.log("set popup false");
+  //     setPopUpVisible(false);
+  //   } else {
+  //     console.log("set popup true");
+  //     setPopUpVisible(true);
+  //   }
+  // }, [user]);
 
   return (
     <div>
-      <Dialog
-        header="Welcome"
-        style={{ width: "auto", margin: 25 }}
-        visible={popUpVisible}
-        closable={false}
-      >
-        <Auth />
-      </Dialog>
-      <Navigation user={user} signOutUser={signOutUser} />
+      {user?.email ? (
+        <Dialog
+          header="Welcome"
+          style={{ width: "auto", margin: 25 }}
+          visible={popUpVisible}
+          closable={false}
+        >
+          <Auth />
+        </Dialog>
+      ) : (
+        <></>
+      )}
+      <Navigation user={user} signOutUser={signOutUser} logIn={logIn} />
       <Routes>
         <Route exact path="/" element={<Cico user={user} />} />
         <Route exact path="/user" element={<User user={user} />} />
@@ -79,32 +83,21 @@ const App = () => {
 const Navigation = (props) => {
   console.log("navigation");
   const navigate = useNavigate();
-  const { user, signOutUser } = props;
+  const { user, signOutUser, logIn } = props;
 
   return (
     <div className="card">
       <div className="card-container blue-container overflow-hidden">
         <div className="flex bg-blue-500 justify-content-end">
-          <div className="flex-initial flex align-items-center justify-content-center font-bold text-white ml-2 mr-5 px-1 pl-1 pr-5 border-round">
-            <Link className="no-underline text-white text-base p-link" to="/">
-              Home
-            </Link>
-          </div>
-          <div className="flex-initial flex align-items-center justify-content-center font-bold text-white m-2 px-1 py-1 border-round">
-            <Link
-              className="no-underline text-white text-base p-link"
-              to="/user"
-            >
-              User
-            </Link>
-          </div>
+          <div className="flex-initial flex align-items-center justify-content-center font-bold text-white ml-2 mr-5 px-1 pl-1 pr-5 border-round"></div>
+          <div className="flex-initial flex align-items-center justify-content-center font-bold text-white m-2 px-1 py-1 border-round"></div>
           <div className="flex-initial flex align-items-end justify-content-end font-bold text-white m-2 px-1 py-1 border-round">
             <div className="card-container">
               <div className="flex-initial flex align-items-center justify-content-end font-bold text-white m-2 px-1 py-1 border-round">
                 <p className="p-link">{user?.email}</p>
               </div>
               <div className="flex-initial flex align-items-center justify-content-end font-bold text-white m-2 px-1 py-1 border-round">
-                {user?.email !== undefined ? (
+                {user !== "" ? (
                   <Button
                     label="Sign out"
                     onClick={() => {
@@ -112,7 +105,7 @@ const Navigation = (props) => {
                     }}
                   />
                 ) : (
-                  <></>
+                  <Button label="Log in" onClick={() => logIn()} />
                 )}
               </div>
               <Button link onClick={() => navigate(-1)}>
